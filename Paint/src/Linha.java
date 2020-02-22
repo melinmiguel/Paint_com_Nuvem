13d42eacb0f8efec264d3b45a8404799e523e7ed
unknown <you@example.com>
2020-02-22 18:22:14 -0300

    Identando e arrumando caracteres bugados

--- Paint/src/Linha.java
+++ Paint/src/Linha.java
@@ -1,86 +1,64 @@
 import java.awt.*;
 import java.util.*;
- 
-public class Linha extends Figura
-{
-    protected Ponto p1, p2;
-    
-    public Linha (int x1, int y1, int x2, int y2)
-    {
-        this (x1, y1, x2, y2, Color.BLACK);
-    }
-	
-    public Linha (int x1, int y1, int x2, int y2, Color cor)
-    {
-        super(cor);
 
-        this.p1 = new Ponto (x1,y1,cor);
-        this.p2 = new Ponto (x2,y2,cor);
-    }
+public class Linha extends Figura {
+  protected Ponto p1, p2;
 
-    public Linha (String s)
-    {
-        StringTokenizer quebrador = new StringTokenizer(s,":");
+  public Linha(int x1, int y1, int x2, int y2) {
+    this(x1, y1, x2, y2, Color.BLACK);
+  }
 
-        quebrador.nextToken();
+  public Linha(int x1, int y1, int x2, int y2, Color cor) {
+    super(cor);
 
-        int   x1  = Integer.parseInt(quebrador.nextToken());
-        int   y1  = Integer.parseInt(quebrador.nextToken());
+    this.p1 = new Ponto(x1, y1, cor);
+    this.p2 = new Ponto(x2, y2, cor);
+  }
 
-        int   x2  = Integer.parseInt(quebrador.nextToken());
-        int   y2  = Integer.parseInt(quebrador.nextToken());
+  public Linha(String s) {
+    StringTokenizer quebrador = new StringTokenizer(s, ":");
 
-        Color cor = new Color (Integer.parseInt(quebrador.nextToken()),  // R
-                               Integer.parseInt(quebrador.nextToken()),  // G
-                               Integer.parseInt(quebrador.nextToken())); // B
+    quebrador.nextToken();
 
-        this.p1  = new Ponto (x1,y1,cor);
-        this.p2  = new Ponto (x2,y2,cor);
-        this.cor = cor;
-    }
+    int x1 = Integer.parseInt(quebrador.nextToken());
+    int y1 = Integer.parseInt(quebrador.nextToken());
 
-    public void setP1 (int x, int y)
-    {
-        this.p1 = new Ponto (x,y,this.getCor());
-    }
+    int x2 = Integer.parseInt(quebrador.nextToken());
+    int y2 = Integer.parseInt(quebrador.nextToken());
 
-    public void setP2 (int x, int y)
-    {
-        this.p2 = new Ponto (x,y,this.getCor());
-    }
+    Color cor = new Color(Integer.parseInt(quebrador.nextToken()), // R
+        Integer.parseInt(quebrador.nextToken()), // G
+        Integer.parseInt(quebrador.nextToken())); // B
 
-    public Ponto getP1 ()
-    {
-        return this.p1;
-    }
+    this.p1 = new Ponto(x1, y1, cor);
+    this.p2 = new Ponto(x2, y2, cor);
+    this.cor = cor;
+  }
 
-    public Ponto getP2 ()
-    {
-        return this.p2;
-    }
+  public void setP1(int x, int y) {
+    this.p1 = new Ponto(x, y, this.getCor());
+  }
 
-    public void torneSeVisivel (Graphics g)
-    {
-        g.setColor(this.cor);
-        g.drawLine(this.p1.getX(), this.p1.getY(),   // ponto inicial
-                   this.p2.getX(), this.p2.getY());  // ponto final
-    }
+  public void setP2(int x, int y) {
+    this.p2 = new Ponto(x, y, this.getCor());
+  }
 
-    public String toString()
-    {
-        return "r:" +
-               this.p1.getX() +
-               ":" +
-               this.p1.getY() +
-               ":" +
-               this.p2.getX() +
-               ":" +
-               this.p2.getY() +
-               ":" +
-               this.getCor().getRed() +
-               ":" +
-               this.getCor().getGreen() +
-               ":" +
-               this.getCor().getBlue();
-    }
+  public Ponto getP1() {
+    return this.p1;
+  }
+
+  public Ponto getP2() {
+    return this.p2;
+  }
+
+  public void torneSeVisivel(Graphics g) {
+    g.setColor(this.cor);
+    g.drawLine(this.p1.getX(), this.p1.getY(), // ponto inicial
+        this.p2.getX(), this.p2.getY()); // ponto final
+  }
+
+  public String toString() {
+    return "r:" + this.p1.getX() + ":" + this.p1.getY() + ":" + this.p2.getX() + ":" + this.p2.getY() + ":"
+        + this.getCor().getRed() + ":" + this.getCor().getGreen() + ":" + this.getCor().getBlue();
+  }
 }
--- Paint/src/Ponto.java
+++ Paint/src/Ponto.java
@@ -1,74 +1,56 @@
 import java.awt.*;
 import java.util.*;
- 
-public class Ponto extends Figura
-{
-    protected int x,  y;
 
-    public Ponto (int x, int y)
-    {
-        this (x, y, Color.BLACK);
-    }
-	  
-    public Ponto (int x, int y, Color cor)
-    {
-        super (cor);
+public class Ponto extends Figura {
+  protected int x, y;
 
-        this.x = x;
-        this.y = y;
-    }
+  public Ponto(int x, int y) {
+    this(x, y, Color.BLACK);
+  }
 
-    public Ponto (String s)
-    {
-        StringTokenizer quebrador = new StringTokenizer(s,":");
+  public Ponto(int x, int y, Color cor) {
+    super(cor);
 
-        quebrador.nextToken();
+    this.x = x;
+    this.y = y;
+  }
 
-        this.x = Integer.parseInt(quebrador.nextToken());
-        this.y = Integer.parseInt(quebrador.nextToken());
+  public Ponto(String s) {
+    StringTokenizer quebrador = new StringTokenizer(s, ":");
 
-        this.cor = new Color (Integer.parseInt(quebrador.nextToken()),  // R
-                              Integer.parseInt(quebrador.nextToken()),  // G
-                              Integer.parseInt(quebrador.nextToken())); // B
-    }
+    quebrador.nextToken();
 
-    public void setX (int x)
-    {
-        this.x = x;
-    }
-	  
-    public void setY (int y)
-    {
-        this.y = y;
-    }
-	  
-    public int getX ()
-    {
-        return this.x;
-    }
-	  
-    public int getY ()
-    {
-    	return this.y;
-    }
-	  
-    public void torneSeVisivel (Graphics g)
-    {
-    	g.setColor (this.cor);
-    	g.drawLine (this.x,this.y,this.x,this.y);
-    }
+    this.x = Integer.parseInt(quebrador.nextToken());
+    this.y = Integer.parseInt(quebrador.nextToken());
 
-    public String toString()
-    {
-        return "p:" +
-               this.x +
-               ":" +
-               this.y +
-               ":" +
-               this.getCor().getRed() +
-               ":" +
-               this.getCor().getGreen() +
-               ":" +
-               this.getCor().getBlue();
-    }
+    this.cor = new Color(Integer.parseInt(quebrador.nextToken()), // R
+        Integer.parseInt(quebrador.nextToken()), // G
+        Integer.parseInt(quebrador.nextToken())); // B
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public int getX() {
+    return this.x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public void torneSeVisivel(Graphics g) {
+    g.setColor(this.cor);
+    g.drawLine(this.x, this.y, this.x, this.y);
+  }
+
+  public String toString() {
+    return "p:" + this.x + ":" + this.y + ":" + this.getCor().getRed() + ":" + this.getCor().getGreen() + ":"
+        + this.getCor().getBlue();
+  }
 }
