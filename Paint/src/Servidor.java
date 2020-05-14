01b1846caa36c146ed6fb01899f554008fd508c8
pedroaranha1 <pedro.aranha@outlook.com>
2020-05-14 19:35:34 -0300

    Create Servidor.java

---
+++ Paint/src/Servidor.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+import java.util.*; 
+public class Servidor {
+    public static void main(String args[]) throws Exception {
+        String clientSentence;
+        String clientNome;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+    
+    while(true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            clientNome = inFromClient.readLine();
+            outToClient.writeBytes("online");
+               
+        }
+    }
+}
