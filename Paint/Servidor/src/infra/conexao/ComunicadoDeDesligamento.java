04c5e1caac4290f035bdc63b4d6ad29061ff1a14
Rodrigo Silva <rodrigo.silva@geofusion.com>
2020-05-18 18:28:22 -0300

    retirando servidor

--- Paint/Servidor/src/infra/conexao/ComunicadoDeDesligamento.java
+++
@@ -1,9 +0,0 @@
-package src.infra.conexao;
-
-public class ComunicadoDeDesligamento extends Comunicado {
-
-  /**
-   *
-   */
-  private static final long serialVersionUID = -6824618674630442453L;
-}
--- Paint/Servidor/src/infra/conexao/PedidoParaSair.java
+++
@@ -1,9 +0,0 @@
-package src.infra.conexao;
-
-public class PedidoParaSair extends Comunicado {
-
-  /**
-   *
-   */
-  private static final long serialVersionUID = 1L;
-}
