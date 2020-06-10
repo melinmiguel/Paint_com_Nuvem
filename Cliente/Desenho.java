353ac0515814c3c75f82cb0c47dbc503f6ab694a
Leonardo Miguel <leonardo.miguel@dextra-sw.com>
2020-06-10 19:47:11 -0300

    inicio do javadoc

--- Cliente/Desenho.java
+++ Cliente/Desenho.java
@@ -1,11 +1,29 @@
 import java.util.ArrayList;
 
+	/**
+	 * Construtor do objeto desenho, objeto que vai armazenar os dados dos desenhos.
+	 * 
+	 * @author    Leonardo
+	 * @author    Rodrigo
+	 * @author    Pedro
+	 */
 public class Desenho {
   private String nome;
   private String dataCriacao;
   private String dataUltimaAtualizacao;
   private ArrayList<String> conteudo;
-
+  
+  /**
+	  * Construtor do objeto desenho, objeto que vai armazenar os dados dos desenhos.
+	 * 
+   * 
+   * @param nom       Nome do desenho
+   * @param dtCri     Data de criação do desenho
+   * @param dtUltAtu  Data de atualização do desenho.
+	 * @author          Leonardo
+	 * @author          Rodrigo
+	 * @author          Pedro
+	 */
   public Desenho(String nom, String dtCri, String dtUltAtu) {
     nome = nom;
     dataCriacao = dtCri;
@@ -17,22 +35,64 @@ public class Desenho {
   // herdadas da classe Figura por exemplo, r:11:22:33:44:55:66:77, para uma
   // linha que vai do ponto com coordenada 11,22 ao ponto com coordenada 33,44
   // e com cor 55:66:77 (55 de red, 66 de green e 77 de blue).
+  
+  /**
+	 *Faz adição do desenho na string.
+	 * 
+   * @param   fig     String da figura
+	 * @author          Leonardo
+	 * @author          Rodrigo
+	 * @author          Pedro
+	 */
   public void addFigura(String fig) {
     conteudo.add(fig);
   }
 
+  /**
+	 *Retorna a quantidade de desenhos na pilha.
+	 * 
+   * @return    Quantidade de desenhos.
+	 * @author    Leonardo
+	 * @author    Rodrigo
+	 * @author    Pedro
+	 */
   public int getQtd() {
     return conteudo.size();
   }
 
+  /**
+	 *Retorna uma figura.
+	 * 
+   * @param i   Numero da figura desejada
+   * @return    Quantidade de desenhos.
+	 * @author    Leonardo
+	 * @author    Rodrigo
+	 * @author    Pedro
+	 */
   public String getFigura(int i) {
     return conteudo.get(i);
   }
 
+  /**
+	 *Retorna o nome do objeto Desenho
+	 * 
+   * @return    nome do objeto.
+	 * @author    Leonardo
+	 * @author    Rodrigo
+	 * @author    Pedro
+	 */
   public String getNome() {
     return this.nome;
   }
 
+  /**
+	 *Retorna a data de criação do objeto.
+	 * 
+   * @return    Data de criação.
+	 * @author    Leonardo
+	 * @author    Rodrigo
+	 * @author    Pedro
+	 */
   public String getDataCriacao() {
     return this.dataCriacao;
   }
