fe850e2afef657e2328c0def28006be32ffa864c
pedroaranha1 <pedro.aranha@outlook.com>
2020-06-15 17:17:33 -0300

    Update BDSQLServer.java
    New config

--- bd/BDSQLServer.java
+++ bd/BDSQLServer.java
@@ -16,7 +16,7 @@ public class BDSQLServer
             comando =
             new MeuPreparedStatement (
             "com.microsoft.sqlserver.jdbc.SQLServerDriver",
-            "jdbc:sqlserver://projetoc.database.windows.net:3306;databasename=projetoc",
+            "jdbc:sqlserver://projetoc.database.windows.net:1433;databasename=projetoc",
             "admpedro", "adm123!@#");
         }
         catch (Exception erro)
@@ -28,3 +28,7 @@ public class BDSQLServer
         COMANDO = comando;
     }
 }
+
+/*jdbc:sqlserver://projetoc.database.windows.net:1433;database=projetoC;user=admpedro@projetoc;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+
+*/
